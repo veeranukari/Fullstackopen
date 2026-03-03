af8548aa8de99dc8e1b4175f5e78ddf754fab03e
veeranukari <157691199+veeranukari@users.noreply.github.com>
2026-03-03 17:51:32 +0200

    Osan 1 tehtävät

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + ' %'} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+  
+
+export default App
---
+++ uusisovellus/src/App.jsx
@@ -0,0 +1,61 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part key={part.name} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  )
+
+  return <p>Number of exercises {total}</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+
+}
+
+export default App
