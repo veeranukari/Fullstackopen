4db31a74789206f66d598be89759f3b20dcc8bf9
veeranukari <157691199+veeranukari@users.noreply.github.com>
2026-04-18 13:40:28 +0300

    tehtävä 2.12

--- osa2/puhelinluettelo/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/puhelinluettelo/src/App.jsx
@@ -1,24 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
- 
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1231244"  }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
- 
+
+  const baseUrl = "http://localhost:3001/persons";
+
+  useEffect(() => {
+    axios.get(baseUrl).then(response => {
+      setPersons(response.data);
+    });
+  }, []);
+
   const addPerson = (event) => {
     event.preventDefault();
+
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    
-    setPersons(persons.concat({ name: newName, number: newNumber }));
-    setNewName("");
-    setNewNumber("");
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    axios.post(baseUrl, personObject).then(response => {
+      setPersons(persons.concat(response.data));
+      setNewName("");
+      setNewNumber("");
+    });
   };
- 
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -37,7 +54,7 @@ const App = () => {
     </div>
   );
 };
- 
+
 export default App;
  
  
