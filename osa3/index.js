f0faa6dfaf0ae2cff4c625f65be273f17c24cb88
veeranukari <157691199+veeranukari@users.noreply.github.com>
2026-04-25 14:19:10 +0300

    const

--- osa3/index.js
+++ osa3/index.js
@@ -9,7 +9,7 @@ app.use(morgan("tiny"));
 app.use(express.static('dist'))
 
 
-let persons = [
+const persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
