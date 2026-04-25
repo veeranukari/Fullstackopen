43a4b61cef95dd3f4dec149beea8cc240d0e1170
veeranukari <157691199+veeranukari@users.noreply.github.com>
2026-04-25 12:43:13 +0300

    tehtävä 3.9

--- osa3/index.js
+++ osa3/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 
 const PORT = 3001;
 
