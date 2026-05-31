83aeb95da59f439d920557664749e660d6bf5ebf
veeranukari <157691199+veeranukari@users.noreply.github.com>
2026-05-31 13:45:59 +0300

    osa5

--- osa5/bloglist-frontend/src/components/Notification.jsx
+++ osa5/bloglist-frontend/src/components/Notification.jsx
@@ -1,12 +1,25 @@
+import styled from 'styled-components'
+
+const Message = styled.div`
+  background: #e3fcef;
+  border: 1px solid #57ae5b;
+  border-left: 6px solid #2f8132;
+  border-radius: 6px;
+  color: #1b5e20;
+  font-weight: 700;
+  margin-bottom: 20px;
+  padding: 12px 16px;
+`
+
 const Notification = ({ message }) => {
   if (message === null) {
     return null
   }
 
   return (
-    <div>
+    <Message>
       {message}
-    </div>
+    </Message>
   )
 }
 
